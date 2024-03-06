06837778af6dc05a3f54793a052724e5ac3b987c
BabiJeypee <150770301+BabiJeypee@users.noreply.github.com>
2024-03-06 22:22:05 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Create an XMLHttpRequest to fetch products data
+let http = new XMLHttpRequest();
+http.open('get', 'products.json', true);
+http.send();
+
+http.onload = function(){
+    // Check if the request is successful and process the data
+    if(this.readyState == 4 && this.status == 200) {
+        let products = JSON.parse(this.responseText);
+        let output ="";
+        
+        // Iterate through products and generate HTML output
+        for(let item of products){
+            output += `
+            <div class="product">
+                <img src="${item.image}" alt="${item.image}">
+                <p class="title">${item.title}</p>
+                <p class="description">${item.description}</p>
+                <p class="price">
+                    <span>${item.price}</span>
+                    <span>&euro;</span>
+                </p>
+                <p class="cart">Add to cart<box-icon name='cart-alt'></box-icon></p>
+            </div>
+            `;    
+        }
+        // Set the generated HTML to the products container
+        document.querySelector('.products').innerHTML = output;
+    }
+}
